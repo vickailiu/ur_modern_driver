f9dd18758b77d70794ab4b382810cdd9d67ad7d1
vickailiu <vic.kai.liu@gmail.com>
2018-05-31 10:01:36 +0800

    fixed the conflict

--- src/ur_ros_wrapper.cpp
+++ src/ur_ros_wrapper.cpp
@@ -61,10 +61,6 @@
 
 // LK modify
 #include "std_msgs/Float32.h"
-<<<<<<< HEAD
-=======
-#include "emma_commons/DoubleArray.h"
->>>>>>> fbc1e6992d14e096eb72156ab80079a5f90ee88e
 
 class RosWrapper {
 protected:
