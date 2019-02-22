4cb0c94e56dcc153969a1dbe30c9ad6a304f18c7
vickailiu <vic.kai.liu@gmail.com>
2019-02-22 17:27:52 +0800

    WIP

--- include/ur_modern_driver/ros/dashboard_service.h
+++ include/ur_modern_driver/ros/dashboard_service.h
@@ -15,6 +15,49 @@ private:
 
   bool sendDashboardCmd(emma_commons::URDashboardRequest& req, emma_commons::URDashboardResponse& resp)
   {
+    addrinfo info;
+
+    memset(&info, 0, sizeof(info));
+    info.ai_family = AF_UNSPEC;
+    info.ai_socktype = SOCK_STREAM;
+
+    addrinfo* res = 0;
+
+    if(getaddrinfo("192.168.0.49", "29999", &info, &res) != 0 || !res)
+    {
+      perror("Could not get address for UR host");
+      return false;
+    }
+
+    sockaddr_storage addr;
+    socklen_t addrlen = res->ai_addrlen;
+    memcpy(&addr, res->ai_addr, res->ai_addrlen);
+
+    int fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    freeaddrinfo(res);
+
+    if(fd < 0)
+    {
+      perror("socket");
+      return false;
+    }
+
+    if(connect(fd, (const sockaddr*)&addr, addrlen) != 0)
+    {
+      perror("Could not connect to UR dashboard");
+      return false;
+    }
+
+    if(write(fd, req.cmd.c_str(), strlen(req.cmd.c_str())) != strlen(req.cmd.c_str()))
+    {
+      perror("Could not write to UR dashboard");
+      return false;
+    }
+
+    close(fd);
+
+
     LOG_INFO("sendDashboardCmd called");
     return (resp.result = commander_.sendDashboardCmd(req.cmd));
   }
