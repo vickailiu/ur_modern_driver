3d50f2cfaccf3daf3768360b222b07e07220b827
XieShaosong <834031119@qq.com>
2019-04-02 14:15:26 +0800

    add the emma log system

--- include/ur_modern_driver/log.h
+++ include/ur_modern_driver/log.h
@@ -3,12 +3,38 @@
 
 #ifdef ROS_BUILD
 #include <ros/ros.h>
+#include "emma_commons/log.h"
 
-#define LOG_DEBUG ROS_DEBUG
-#define LOG_WARN ROS_WARN
-#define LOG_INFO ROS_INFO
-#define LOG_ERROR ROS_ERROR
-#define LOG_FATAL ROS_FATAL
+#define LOG_DEBUG(format...) \
+{\
+  char info[512]; \
+  sprintf(info, format);\
+  emma_commons::Log::EMMA_INFO(__FILE__, __LINE__, info, "ur_driver", true);\
+}
+#define LOG_ERROR(format...)\
+{\
+  char err[512];\
+  sprintf(err, format);\
+  emma_commons::Log::EMMA_ERROR(__FILE__, __LINE__, err, "ur_driver");\
+}
+#define LOG_INFO(format...) \
+{\
+  char info[512]; \
+  sprintf(info, format);\
+  emma_commons::Log::EMMA_INFO(__FILE__, __LINE__, info, "ur_driver", true);\
+}
+#define LOG_WARN(format...) \
+{\
+  char info[512]; \
+  sprintf(info, format);\
+  emma_commons::Log::EMMA_INFO(__FILE__, __LINE__, info, "ur_driver", true);\
+}
+#define LOG_FATAL(format...) \
+{\
+  char info[512]; \
+  sprintf(info, format);\
+  emma_commons::Log::EMMA_INFO(__FILE__, __LINE__, info, "ur_driver", true);\
+}
 
 #else
 
